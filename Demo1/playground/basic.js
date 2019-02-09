c294197df26d68f4f2428a28e0d31d936af0e38c
ashu7575 <ashu7575@gmail.com>
2019-02-10 00:09:53 +0530

    quick fix

---
+++ Demo1/playground/basic.js
@@ -0,0 +1,11 @@
+console.log("starting app");
+
+setTimeout(() => {
+    console.log("in function");
+}, 2000);
+
+setTimeout(() => {
+    console.log("in function 2");
+}, 0);
+
+console.log("Ending..");
