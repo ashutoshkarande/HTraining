33dfbb4260cf6900a05ebe7c6af2070df99ca1af
ashu7575 <ashu7575@gmail.com>
2019-03-03 23:36:46 +0530

    test

---
+++ Demo2/app.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=J0IaStfFYH3kbf27YV2yLEZkAVXEPe0d&location=sainagar%20pune',
+    json: true
+}, (error, response, body) => {
+    console.log(body);
+    
+});
